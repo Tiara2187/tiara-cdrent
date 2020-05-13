55ceaf69fcc3a27697fe4e64c81baf417921ea2d
Tiara2187 <tiaramuttianingtyas23@gmail.com>
2020-05-13 15:11:57 +0700

    Membuat route untuk register dan login user

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,26 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+use Illuminate\Http\Request;
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+$router->post("/register", "AuthController@register");
+$router->post("/login", "AuthController@login");
+
+$router->get('/cd', 'CdController@index');
+$router->get('/cd/{id}', 'CdController@show');
+$router->post('/cd', 'CdController@store');
+$router->put('/cd/{id}', 'CdController@update');
+$router->delete('/cd/{id}', 'CdController@destroy');
+
+$router->group(['middleware' => 'auth'], function() use ($router) {
+    $router->get('/user', function() {
+        return Auth::user();
+    });
+
+    $router->post('/rent/start', 'RentController@startRent');
+    $router->post('/rent/end', 'RentController@endRent');
 });
